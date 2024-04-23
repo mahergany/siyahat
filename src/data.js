ff79967834d6abf047e778a1e68851994e9e7acd
eashahemaan <ebajwa.bscs22seecs@seecs.edu.pk>
2024-04-23 15:30:58 +0500

    Implemented parallax front page

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const imageSlide = [
+
+    {
+        url:'src/Images/logo.jpg',
+        title: '1',
+        body:'FK.SD'
+
+    },
+
+    { 
+        url:'src/Images/logo.jpg',
+        title: '2',
+        body:'FK.SD'
+    },
+
+    { 
+        url:'src/Images/logo.jpg',
+        title: '3',
+        body:'FK.SD'
+    }
+
+]
+
+export default imageSlide;
