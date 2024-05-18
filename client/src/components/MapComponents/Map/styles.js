1e06e250272706d07abc6612a3d730f448580d18
eashahemaan <ebajwa.bscs22seecs@seecs.edu.pk>
2024-05-18 17:34:08 +0500

    work on friendlistwidget+profilepage+posts

--- client/src/components/MapComponents/Map/styles.js
+++ client/src/components/MapComponents/Map/styles.js
@@ -1,8 +1,11 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
-  paper: {
+  paperAttractions: {
     padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px', backgroundColor: 'pink'
+  },
+  paperRestaurants:{
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px', backgroundColor: '#E8A90A'
   },
   mapContainer: {
     // height: '85vh', width: '100%',
