886138d8a604134d922bb7f6068f7034cae5c5aa
mahergany <mahmed.bscs22seecs@seecs.edu.pk>
2024-05-18 09:28:27 +0500

    fixed save and like

--- server/routes/savedPost.js
+++ server/routes/savedPost.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { getSavedPostsFromUserId, savePost } from "../controllers/savedPost.js";
+import { getSavedPostsFromUserId, savePost, getIsSavedFromUserId } from "../controllers/savedPost.js";
 
 const router = express.Router();
 
 /* READ */
-router.get("/:userId/", verifyToken, getSavedPostsFromUserId);
+router.get("/:userId", verifyToken, getSavedPostsFromUserId);
 
 /* UPDATE */
 router.post("/:userId",verifyToken,savePost );
+router.post("/isSaved/:userId",verifyToken, getIsSavedFromUserId );
 
 export default router;
